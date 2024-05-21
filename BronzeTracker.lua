d68540e0f9296eb5e4f16a9b2888c825ce2d0758
Kristian Williams <kristian.andersonwilliams@gmail.com>
2024-05-21 16:41:15 +0200

    Update BronzeTracker.lua

--- BronzeTracker.lua
+++ BronzeTracker.lua
@@ -1,32 +1,18 @@
-local _, addonTable = ...;
+local ADDON_NAME, namespace = ...;
 
-addonTable.BronzeTracker = {};
-
-local BronzeTracker = addonTable.BronzeTracker;
+local BronzeTracker = namespace[ADDON_NAME];
+BronzeTracker = {};
 BronzeTracker.debugging = true;
 BronzeTracker.currencyId = 2778;
 BronzeTracker.maximumRequired = 1607500;
 BronzeTracker.loaded = false;
 
-BronzeTracker.items = {};
-BronzeTracker.items.mounts = {
-    { itemID = 441794, cost = 38500 },
-    { itemID = 127158, cost = 38500 },
-    { itemID = 127170, cost = 18700 },
-    { itemID = 148476, cost = 38500 },
-    { itemID = 435084, cost =  2200 }
-};
+namespace.Mounts = namespace.Mounts or {};
+namespace.Toys = namespace.Toys or {};
+namespace.Appearances = namespace.Appearances or {};
 
-BronzeTracker.items.toys = {
-    { itemID = 104309, cost = 50000 }
-};
-
-BronzeTracker.items.appearances = {
-    { itemID = 104309, cost =  2500 }
-};
-
-function addonTable.BronzeTracker:print(message)
-    if (addonTable.BronzeTracker.debugging) then
+function BronzeTracker:print(message)
+    if (BronzeTracker.debugging) then
         print(message)
     end
 end
@@ -35,20 +21,20 @@ function BronzeTracker:GetTotalRequired()
     local currentTotal = C_CurrencyInfo.GetCurrencyInfo(BronzeTracker.currencyId);
     local maximumRequired = 0;
 
-    for _, mount in pairs(BronzeTracker.items.mounts) do
+    for _, mount in pairs(namespace.Mounts) do
         local _, _, _, _, _, _, _, _, _, _, isCollected = C_MountJournal.GetMountInfoByID(mount.itemID);
         if (isCollected ~= true) then
             maximumRequired = maximumRequired + mount.cost;
         end
     end
 
-    for _, toy in pairs(BronzeTracker.items.toys) do
+    for _, toy in pairs(namespace.Toys) do
         if (PlayerHasToy(toy.itemID) ~= true) then
             maximumRequired = maximumRequired + toy.cost;
         end
     end
 
-    for _, appearance in pairs(BronzeTracker.items.appearances) do
+    for _, appearance in pairs(namespace.Appearances) do
         if (C_TransmogCollection.PlayerHasTransmogByItemInfo(appearance.itemID) ~= true) then
             maximumRequired = maximumRequired + appearance.cost;
         end
