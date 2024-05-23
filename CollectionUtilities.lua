071afaa2f09ed692ade3c6b1bd2017a6c790b198
Kristian Williams <kristian@softtech.nl>
2024-05-23 11:24:53 +0200

    - Added attempts to find ensemble/arsenal transmogs by tooltip and spell.

--- CollectionUtilities.lua
+++ CollectionUtilities.lua
@@ -3,6 +3,8 @@ local ADDON_NAME, namespace = ...;
 local CollectionUtilities = namespace.CollectionUtilities;
 local Logger, LogLevel, Strings = namespace.Logger, namespace.Logger.LogLevel, namespace.Strings;
 
+local findTransmogBySpell = true;
+
 CollectionUtilities = {};
 
 function CollectionUtilities:PlayerHasMount(itemID)
@@ -34,47 +36,81 @@ function CollectionUtilities:PlayerHasToy(itemID)
     end
 end
 
-function CollectionUtilities:PlayerHasAppearance(itemID)
+local function findAppearanceByTransmog(itemID)
+    Logger:Print(LogLevel.VERBOSE, Strings:Get("CHECKING_DRESSABLE_ITEM"), itemID);
+
+    local itemAppearanceID, _ = C_TransmogCollection.GetItemInfo(itemID);
     local itemName = C_Item.GetItemInfo(itemID);
 
-    if (C_Item.IsDressableItemByID(itemID)) then
-        Logger:Print(LogLevel.VERBOSE, Strings:Get("CHECKING_DRESSABLE_ITEM"), itemID);
+    if (itemName == nil or itemAppearanceID == nil) then
+        Logger:Print(LogLevel.WARNING, Strings:Get("WARNING_INVALID_APPEARANCE"), itemID);
 
-        local itemAppearanceID, _ = C_TransmogCollection.GetItemInfo(itemID);
-
-        if (itemName == nil or itemAppearanceID == nil) then
-            Logger:Print(LogLevel.WARNING, Strings:Get("WARNING_INVALID_APPEARANCE"), itemID);
-
-            return nil;
-        else
-            local collected = C_TransmogCollection.PlayerHasTransmogByItemInfo(itemID);
-            Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(collected));
-
-            return collected;
-        end
+        return nil;
     else
-        Logger:Print(LogLevel.VERBOSE, Strings:Get("CHECKING_ENSEMBLE_ITEM"), itemID);
+        local collected = C_TransmogCollection.PlayerHasTransmogByItemInfo(itemID);
+        Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(collected));
 
-        local tooltip = C_TooltipInfo.GetItemByID(itemID);
+        return collected;
+    end
+end
 
-        if (tooltip == nil) then
-            return nil;
+local function findAppearanceBySpell(itemID)
+    Logger:Print(LogLevel.VERBOSE, Strings:Get("CHECKING_ENSEMBLE_ITEM"), itemID);
+
+    local _, spellID = C_Item.GetItemSpell(itemID);
+
+    if (spellID == nil) then
+        Logger:Print(LogLevel.WARNING, Strings:Get("WARNING_INVALID_APPEARANCE"), itemID);
+
+        return nil;
+    end
+
+    local collected = IsUsableSpell(spellID) == false;
+    local itemName = C_Item.GetItemInfo(itemID);
+
+    Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(collected));
+
+    return collected;
+end
+
+local function findAppearanceByTooltip(itemID)
+    Logger:Print(LogLevel.VERBOSE, Strings:Get("CHECKING_ENSEMBLE_ITEM"), itemID);
+
+    local tooltip = C_TooltipInfo.GetItemByID(itemID);
+
+    if (tooltip == nil) then
+        Logger:Print(LogLevel.WARNING, Strings:Get("WARNING_INVALID_APPEARANCE"), itemID);
+
+        return nil;
+    end
+
+    local itemName = C_Item.GetItemInfo(itemID);
+    for index, line in ipairs(tooltip.lines) do
+        if (line.leftText and string.match(line.leftText, ITEM_SPELL_KNOWN)) then
+            Logger:Print(LogLevel.VERBOSE, Strings:Get("FOUND_ITEM_TOOLTIP_TEXT"), "ITEM_SPELL_KNOWN", index, "leftText", line.tooltipType);
+
+            Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(true));
+            return true;
         end
 
-        for index, line in ipairs(tooltip.lines) do
-            if (line.leftText and string.match(line.leftText, ITEM_SPELL_KNOWN)) then
-                Logger:Print(LogLevel.VERBOSE, Strings:Get("FOUND_ITEM_TOOLTIP_TEXT"), "ITEM_SPELL_KNOWN", index, "leftText", line.tooltipType);
+        if (line.rightText and string.match(line.rightText, ITEM_SPELL_KNOWN)) then
+            Logger:Print(LogLevel.VERBOSE, Strings:Get("FOUND_ITEM_TOOLTIP_TEXT"), "ITEM_SPELL_KNOWN", index, "rightText", line.tooltipType);
 
-                return true;
-            end
-
-            if (line.rightText and string.match(line.rightText, ITEM_SPELL_KNOWN)) then
-                Logger:Print(LogLevel.VERBOSE, Strings:Get("FOUND_ITEM_TOOLTIP_TEXT"), "ITEM_SPELL_KNOWN", index, "rightText", line.tooltipType);
-
-                return true;
-            end
+            Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(true));
+            return true;
         end
+    end
 
-        return false;
+    Logger:Print(LogLevel.VERBOSE, Strings:Get("COLLECTED_STATE"), itemName, tostring(false));
+    return false;
+end
+
+function CollectionUtilities:PlayerHasAppearance(itemID)
+    if (C_Item.IsDressableItemByID(itemID)) then
+        return findAppearanceByTransmog(itemID);
+    elseif (findTransmogBySpell == true) then
+        return findAppearanceBySpell(itemID);
+    else
+        return findAppearanceByTooltip(itemID);
     end
 end
